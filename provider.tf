8e57ff0cc59c6c657a88f418403a17c1c4030e4f
leandro <leandro.cavallari123@gmail.com>
2024-05-11 11:43:31 -0300

    feat: added s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region  = var.regionDefault
 }
+
+terraform {
+  backend "s3" {
+    bucket = "food-fusion-bucket"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"  
+  }
+}
